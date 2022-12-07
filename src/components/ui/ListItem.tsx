2bc5fbac1f72af98d49f3079bd23303645ebb5c0
YI <57102971+london-newyork@users.noreply.github.com>
2022-12-07 22:02:22 +0900

    繰り返しをシンプルにするための処理とリストのUIの調整

--- src/components/ui/ListItem.tsx
+++ src/components/ui/ListItem.tsx
@@ -1,24 +1,36 @@
 import { SVG } from '@Commons/SVG';
 import React from 'react';
-import { ListItem } from './ListItem';
 
-export const SideBar = () => {
+type SidebarIcons = {
+  name: string;
+  d: string;
+  title: string;
+}[];
+
+const sidebarIcons: SidebarIcons = [
+  {
+    name: 'dashboard',
+    d: 'M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z',
+    title: 'Dashboard',
+  },
+];
+
+export const ListItem = () => {
   return (
-    <aside className='sidebar'>
-      <div className='sidebar-list-item-wrapper'>
-        <ListItem />
-        {/* <ul className='sidebar-list-item'>
+    <>
+      {sidebarIcons.map((item) => {
+        <ul className='sidebar-list-item'>
           <li>
             <ul className='sidebar-list-item-title'>
               <li className='sidebar-main-icon-wrapper'>
                 <SVG
                   variant='solid'
-                  name='arrow'
-                  dWithRule='M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z'
+                  name={item.name}
+                  dWithRule={item.d}
                   className='icon-6'
                 />
               </li>
-              <li className='sidebar-list-item-text'>Dashboard</li>
+              <li className='sidebar-list-item-text'>{item.title}</li>
             </ul>
           </li>
           <li className='sidebar-icon-open-close'>
@@ -31,9 +43,9 @@ export const SideBar = () => {
               stroke='currentColor'
             />
           </li>
-         
-        </ul> */}
-      </div>
-    </aside>
+          {/* TODO:ここにドロップダウンメニューがはいります */}
+        </ul>;
+      })}
+    </>
   );
 };
