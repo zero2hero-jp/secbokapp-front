0c89b6c3603d4f63a636634e952f215a77050fd4
YI <57102971+london-newyork@users.noreply.github.com>
2022-12-08 17:35:56 +0900

    汎用的なボタンの修正

--- src/components/commons/Button.tsx
+++ src/components/commons/Button.tsx
@@ -2,7 +2,31 @@ import React from 'react';
 type ButtonProps = {
   children: React.ReactNode;
   className: string;
+  type: 'button' | 'submit' | 'reset';
+  value?: string;
+  onClick?: () => void;
+  disabled?: boolean;
+  form?: string;
 };
-export const Button = ({ children, className }: ButtonProps) => {
-  return <button className={className}>{children}</button>;
+export const Button = ({
+  children,
+  className,
+  type,
+  onClick,
+  value,
+  form,
+  disabled,
+}: ButtonProps) => {
+  return (
+    <button
+      className={className}
+      type={type}
+      onClick={onClick}
+      value={value}
+      form={form}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
 };
