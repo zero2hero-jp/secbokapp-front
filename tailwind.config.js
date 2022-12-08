e354db7de3f7bc022cec58481133a448308db96c
YI <57102971+london-newyork@users.noreply.github.com>
2022-12-08 17:35:42 +0900

    実装中に仕様が変わったのでブレイクポイントを修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,8 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
-      md: '638px',
+      md: '639px',
       lg: '1024px',
-      xl: '1536px',
     },
     extend: {},
   },
