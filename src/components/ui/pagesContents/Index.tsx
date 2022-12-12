5f6740a4a1d91c8d8bc2d4149764de440baa2780
YI <57102971+london-newyork@users.noreply.github.com>
2022-12-12 16:46:23 +0900

    git が大文字小文字を区別していなかったため Aboutをaboutとして認識してもらうため変更した。

--- src/components/ui/pagesContents/Index.tsx
+++ src/components/ui/pagesContents/Index.tsx
@@ -10,10 +10,32 @@ type IndexProps = {
 }[];
 
 export const Index = () => {
-  const getUUId = new Date().getTime().toString();
+  const uuId = () => {
+    var uuid = '',
+      i,
+      random;
+    for (i = 0; i < 32; i++) {
+      random = (Math.random() * 16) | 0;
+
+      if (i == 8 || i == 12 || i == 16 || i == 20) {
+        uuid += '-';
+      }
+      uuid += (i == 12 ? 4 : i == 16 ? (random & 3) | 8 : random).toString(16);
+    }
+    return uuid;
+  };
+
   const indexData: IndexProps = [
     {
-      id: getUUId,
+      id: uuId(),
+      title: 'Norway Fjord Adventures',
+      badge: 'On Sale',
+      paragraph:
+        'With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway',
+      btnName: 'Book classic tour now',
+    },
+    {
+      id: uuId(),
       title: 'Norway Fjord Adventures',
       badge: 'On Sale',
       paragraph:
@@ -30,7 +52,7 @@ export const Index = () => {
       {indexData.map((item) => (
         <>
           <Grid key={item.id}>
-            <Grid.Col span={6}>
+            <Grid.Col span={8}>
               <Card shadow='sm' p='lg' radius='xs' withBorder>
                 <Card.Section>{/* <Image alt={} src={} /> */}</Card.Section>
                 <Group position='apart' mt='md' mb='xs'>
